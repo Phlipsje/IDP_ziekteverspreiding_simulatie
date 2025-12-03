2bd596a43877e3cc9e2799b2f6372735de22f581
Philip <philiptap2005@outlook.com>
2025-12-03 11:20:22 +0100

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,9 @@
+//Javascript entry point
+
+window.onload = function(){
+    initialize();
+}
+
+function initialize(){
+    //TODO load in .csv data
+}
---
+++ src/simulation/simulation.js
@@ -0,0 +1,6 @@
+//Entry point for simulation
+
+//Reloads the entire simulation
+function reload(){
+    //TODO load in json (settings data)
+}

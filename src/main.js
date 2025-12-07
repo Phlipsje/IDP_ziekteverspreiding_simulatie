f3a191c38b594cec6b0e050aa2520997ca92c6f3
Philip <philiptap2005@outlook.com>
2025-12-07 17:23:19 +0100

    Added Svelte + Tailwindcss

--- src/main.js
+++
@@ -1,9 +0,0 @@
-//Javascript entry point
-
-window.onload = function(){
-    initialize();
-}
-
-function initialize(){
-    //TODO load in .csv data
-}
--- src/simulation/simulation.js
+++
@@ -1,6 +0,0 @@
-//Entry point for simulation
-
-//Reloads the entire simulation
-function reload(){
-    //TODO load in json (settings data)
-}

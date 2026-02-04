bee5e89442e49fedced2956886bdd20f6136981b
Philip <philiptap2005@outlook.com>
2026-02-04 10:25:49 +0100

    Updated model to reflect age groups, ect added by Sidney. Also small performance improvements on old model, but lost due to age group computations, so more optimizations coming later.

--- src/lib/simulation/simulationData.js
+++ src/lib/simulation/simulationData.js
@@ -15,6 +15,10 @@ let municipalityStats = [];
 const codeToInternalId = new Map();
 const idToCode = new Map();
 
+const AGE_GROUPS = 3;
+//Temp until we have it in dataset
+const ageDistribution = [0.2, 0.5, 0.3]; // 0–17, 18–65, 65+
+
 export function loadDatasets(){
 	const bevolkingParsed = Papa.parse(csvBevolkingText, {header: true}).data;
 	const geografieParsed = Papa.parse(csvGeografieText, {header: true,skipEmptyLines: true}).data;
@@ -64,17 +68,30 @@ export function loadDatasets(){
 
 export function createMunicipalityObjects(){
 	municipalityData.forEach((row, index) => {
+
 		const stats = {
 			id: index,
 			gemeenteCode: row.gemeenteCode,
-			population: row.bevolking,
-			susceptible: row.bevolking,
-			infected: 0,
-			recovered: 0,
-			vaccinated: 0,
-			deaths: 0,
+			population: Array(AGE_GROUPS),
+			susceptible: Array(AGE_GROUPS),
+			infected: Array(AGE_GROUPS),
+			recovered: Array(AGE_GROUPS),
+			vaccinated: Array(AGE_GROUPS),
+			deaths: Array(AGE_GROUPS),
 			distances: getDistances(row.gemeenteCode), //Note that distances will contain itself
+			contactRow: Array(municipalityData.length), //One entry per municipality, pre-made to avoid repeated allocations
 		};
+
+		for (let a = 0; a < AGE_GROUPS; a++) {
+			const groupPop = Math.floor(row.bevolking * ageDistribution[a]);
+
+			stats.population[a] = groupPop;
+			stats.susceptible[a] = groupPop;
+			stats.infected[a] = 0;
+			stats.recovered[a] = 0;
+			stats.vaccinated[a] = 0;
+			stats.deaths[a] = 0;
+		}
 
 		municipalityStats.push(stats);
 	});
@@ -161,6 +178,7 @@ export function municipalityBbox(gemeenteCode) {
 	return m ? { width: m.bboxWidth, height: m.bboxHeight } : null;
 }
 
+/* TEMP not working
 export function municipalitySusceptible(gemeenteCode){
 	return getMunicipalityStats(gemeenteCode)?.susceptible;
 }
@@ -172,6 +190,7 @@ export function municipalityInfected(gemeenteCode){
 export function municipalityRecovered(gemeenteCode){
 	return getMunicipalityStats(gemeenteCode)?.recovered;
 }
+ */
 
 export function municipalityDistances(gemeenteCode){
 	return getMunicipalityStats(gemeenteCode)?.distances;
--- src/lib/simulation/simulationModel.js
+++ src/lib/simulation/simulationModel.js
@@ -5,23 +5,30 @@ import {
 } from './simulationData.js';
 
 const transmissionRate = 0.25;
-const recoveryRate = 0.1;
 const dt = 1.0; //One day per tick
 const contactScaling = 1.3; //Exponent for how many more contacts for bigger municipalities
 const travelFactor = 0.3; //Fraction of population of municipality that travels to every other municipality
 const distanceDecay = 0.9; //travelFactor*(KMs_distance)^this is the effect of distance
-const vaccinationRate = 0.0; //TODO Change to per age group later
-const waningRecoveryRate = 0.0; //Chance for recovered person to become infected again
-const waningVaccinationRate = 0.00;
-const mortalityRate = 0.0;
-const d0 = 0.000001;
+const recoveryRate = [0.12, 0.1, 0.08]; // 0–17, 18–65, 65+
+const waningRecoveryRate = [0.0, 0.0, 0.0]; // 0–17, 18–65, 65+
+const vaccinationRate = [0.0, 0.0, 0.0]; // 0–17, 18–65, 65+
+const waningVaccinationRate = [0.0, 0.0, 0.0]; // 0–17, 18–65, 65+
+const mortalityRate = [0.0001, 0.001, 0.01]; // 0–17, 18–65, 65+
+const d0 = 0.000001; //Miniscule value to not get divide by 0's
+// How age groups mix with each other (rows sum to 1)
+const AGE_GROUPS = 3;
+const ageContactMatrix = [
+	[0.7, 0.25, 0.05],
+	[0.2, 0.7, 0.1],
+	[0.1, 0.3, 0.6],
+];
 
 //Does the setup of stats
 export function startModel(){
 	const amsterdam = getMunicipalityStats("GM0363");
 	const startingInfected = 10;
-	amsterdam.infected = startingInfected;
-	amsterdam.susceptible = amsterdam.susceptible - startingInfected;
+	amsterdam.infected[1] = startingInfected;
+	amsterdam.susceptible[1] = amsterdam.susceptible[1] - startingInfected;
 }
 
 //Runs a step of the simulation
@@ -29,44 +36,47 @@ export function stepModel() {
 	const municipalities = getMunicipalities();
 	const nextState = [];
 
-	//Chances computed every frame in the case dt changes
-	const recoveryChance = probFromRate(recoveryRate, dt);
-	const waningRecoveryChance = probFromRate(waningRecoveryRate, dt);
-	const vaccinationChance = probFromRate(vaccinationRate, dt);
-	const waningVaccinationChance = probFromRate(waningVaccinationRate, dt);
-	const mortalityChance = probFromRate(mortalityRate, dt);
-
 	municipalities.forEach((municipality, i) => {
-		//Contact weights W_ij
-		const Wij = computeContactRow(i, municipalities);
+		const updatedMunicipality = structuredClone(municipality);
 
-		//Force of infection λ_i
-		const infectionChance = forceOfInfection(i, municipalities, Wij);
+		for (let a = 0; a < AGE_GROUPS; a++) {
 
-		///Stochastic transitions
-		//New infections / infection pressure
-		const newInfections = binomialDraw(municipality.susceptible, infectionChance);
-		//Recoveries
-		const newRecoveries = binomialDraw(municipality.infected, recoveryChance);
-		const newWaningRecoveries = binomialDraw(municipality.recovered, waningRecoveryChance);
-		//Vaccinations
-		const newVaccinations = binomialDraw(municipality.susceptible - municipality.infected, vaccinationChance);
-		const newWaningVaccinations = binomialDraw(municipality.vaccinated, waningVaccinationChance);
-		//Deaths
-		const newDeaths = binomialDraw(municipality.infected, mortalityChance);
+			//TODO this part can be moved outside loop with small optimization later
+			//Chances computed every frame in the case dt changes
+			const recoveryChance = probFromRate(recoveryRate[a], dt);
+			const waningRecoveryChance = probFromRate(waningRecoveryRate[a], dt);
+			const vaccinationChance = probFromRate(vaccinationRate[a], dt);
+			const waningVaccinationChance = probFromRate(waningVaccinationRate[a], dt);
+			const mortalityChance = probFromRate(mortalityRate[a], dt);
+
+			//Contact weights W_ij
+			computeContactRow(i, municipalities);
+
+			//Force of infection λ_i
+			const infectionChance = forceOfInfection(i, a, municipalities, municipality.contactRow);
+
+			///Stochastic transitions
+			//New infections / infection pressure
+			const newInfections = binomialDraw(municipality.susceptible[a], infectionChance);
+			//Recoveries
+			const newRecoveries = binomialDraw(municipality.infected[a], recoveryChance);
+			const newWaningRecoveries = binomialDraw(municipality.recovered[a], waningRecoveryChance);
+			//Vaccinations
+			const newVaccinations = binomialDraw(municipality.susceptible[a] - newInfections, vaccinationChance);
+			const newWaningVaccinations = binomialDraw(municipality.vaccinated[a], waningVaccinationChance);
+			//Deaths
+			const newDeaths = binomialDraw(municipality.infected[a], mortalityChance);
+
+			updatedMunicipality.population[a] += -newDeaths;
+			updatedMunicipality.susceptible[a] += -newInfections - newVaccinations + newWaningRecoveries + newWaningVaccinations;
+			updatedMunicipality.infected[a] += newInfections - newRecoveries - newDeaths;
+			updatedMunicipality.recovered[a] += newRecoveries - newWaningRecoveries;
+			updatedMunicipality.vaccinated[a] += newVaccinations - newWaningVaccinations;
+			updatedMunicipality.deaths[a] += newDeaths;
+		}
 
 		//State update
-		nextState.push({
-			id: municipality.id,
-			gemeenteCode: municipality.gemeenteCode,
-			population: municipality.population,
-			susceptible: municipality.susceptible - newInfections - newVaccinations + newWaningRecoveries + newWaningVaccinations,
-			infected: municipality.infected + newInfections - newRecoveries - newDeaths,
-			recovered: municipality.recovered + newRecoveries - newWaningRecoveries,
-			vaccinated: municipality.vaccinated + newVaccinations - newWaningVaccinations,
-			deaths: municipality.deaths + newDeaths,
-			distances: municipality.distances,
-		});
+		nextState.push(updatedMunicipality);
 	});
 
 	setMunicipalities(nextState);
@@ -87,31 +97,40 @@ function probFromRate(rate, dt){
 	return 1 - Math.exp(-rate * dt);
 }
 
-//TODO this can be optimized
 function computeContactRow(i, municipalities) {
-	const raw = [];
 	let sumRaw = 0;
 
 	municipalities.forEach((mj, j) => {
 		const dij = municipalities[i].distances[j];
 		const value =
-			Math.pow(mj.population, contactScaling) /
+			Math.pow(mj.population[0]+mj.population[1]+mj.population[2], contactScaling) /
 			Math.pow(dij + d0, distanceDecay);
 
-		raw.push(value);
+		municipalities[i].contactRow[j] = value;
 		sumRaw += value;
 	});
 
-	return raw.map(v => v / sumRaw);
+	const inv = 1 / sumRaw;
+	for (let j = 0; j < municipalities[i].contactRow.length; j++) {
+		municipalities[i].contactRow[j] *= inv;
+	}
 }
 
-function forceOfInfection(i, municipalities, Wij) {
+function forceOfInfection(i, a, municipalities, Wij) {
 	let lambda = 0;
 
-	//TODO add age groups to this
 	municipalities.forEach((mj, j) => {
-		const infected_fraction = mj.infected / mj.population;
-		lambda += Wij[j] * (infected_fraction);
+		for (let b = 0; b < AGE_GROUPS; b++) {
+			const relevantPop =
+				mj.susceptible[b] +
+				mj.infected[b] +
+				mj.recovered[b] +
+				mj.vaccinated[b];
+
+			const infectedFraction = mj.infected[b] / relevantPop;
+
+			lambda += Wij[j] * ageContactMatrix[a][b] * infectedFraction;
+		}
 	});
 
 	lambda *= transmissionRate;
